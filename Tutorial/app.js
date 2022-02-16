d04646cfa1eb2c16119864478122a47e461fed95
Darania Muralitharan <darania@Daranias-MacBook-Air.local>
2022-02-16 22:45:27 +0000

    dirname, filename, require, module, process and setInterval

--- Tutorial/app.js
+++ Tutorial/app.js
@@ -7,3 +7,11 @@
 // process - info about env where the program is being executed
 
 console.log(__dirname)
+console.log(__filename)
+console.log(require)
+console.log(module)
+console.log(process)
+
+setInterval(() => {
+    console.log('hello world')
+}, 1000)
